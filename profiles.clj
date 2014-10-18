0cfee3f38161f3ae9e3ff376b156fe5856c48224
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2014-10-18 20:53:30 +0300

    Update vinyasa

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,10 @@
-{:user {:dependencies [[im.chit/vinyasa "0.1.8"]
-                       [difform "1.1.2"]
+{:user {:dependencies [[spyscope "0.1.4"]
                        [org.clojure/tools.namespace "0.2.4"]
-                       [spyscope "0.1.4"]]
+                       [im.chit/vinyasa "0.2.2"]
+                       [im.chit/iroh "0.1.11"]
+                       [io.aviso/pretty "0.1.8"]
+                       [leiningen #=(leiningen.core.main/leiningen-version)]
+                       [difform "1.1.2"]]
         :plugins [[lein-localrepo "0.5.3"]
                   [jonase/eastwood "0.1.4"]
                   [cider/cider-nrepl "0.8.0-SNAPSHOT"]
@@ -19,11 +22,17 @@
                         cider.nrepl.middleware.trace/wrap-trace
                         cider.nrepl.middleware.undef/wrap-undef]}
         ;:cljsbuild {:builds {:dev {:notify-command ["growlnotify" "-m"]}}}
-        :injections [(require 'vinyasa.inject 'com.georgejahad.difform 'spyscope.core)
-                     (vinyasa.inject/inject 'clojure.core '>
-                                            '[[clojure.repl doc source pst]
-                                              [clojure.pprint pprint pp]
-                                              [com.georgejahad.difform difform]
-                                              [clojure.java.javadoc javadoc]
-                                              [clojure.tools.namespace.repl refresh]
-                                              [vinyasa.pull pull]])]}}
+        :injections [(require 'com.georgejahad.difform)
+                     (require 'spyscope.core)
+                     (require '[vinyasa.inject :as inject])
+                     (require 'io.aviso.repl)
+                     (inject/in
+                      [vinyasa.inject :refer [inject [in inject-in]]]
+                      [vinyasa.lein :exclude [*project*]]
+                      [vinyasa.pull :all]
+                      [cemerick.pomegranate add-classpath get-classpath resources]
+                      clojure.core
+                      [iroh.core .> .? .* .% .%>]
+                      clojure.core >
+                      [clojure.pprint pprint]
+                      [clojure.java.shell sh])]}}
