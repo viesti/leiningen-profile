698084cd73e741f3f949fb78a5de9854e0de3c40
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2014-11-24 09:35:40 +0200

    No need to list individual middlewares

--- profiles.clj
+++ profiles.clj
@@ -1,16 +1,5 @@
 {:shared {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]
-          :repl-options {:init (set! *print-length* 42)
-                         :nrepl-middleware
-                         [cider.nrepl.middleware.classpath/wrap-classpath
-                          cider.nrepl.middleware.complete/wrap-complete
-                          cider.nrepl.middleware.info/wrap-info
-                          cider.nrepl.middleware.inspect/wrap-inspect
-                          cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                          cider.nrepl.middleware.resource/wrap-resource
-                          cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                          cider.nrepl.middleware.test/wrap-test
-                          cider.nrepl.middleware.trace/wrap-trace
-                          cider.nrepl.middleware.undef/wrap-undef]}}
+          :repl-options {:init (set! *print-length* 42)}}
  :vinyasa {:dependencies [[im.chit/vinyasa "0.2.2"]
                           [im.chit/iroh "0.1.11"]]
            :injections [(require '[vinyasa.inject :as inject])
