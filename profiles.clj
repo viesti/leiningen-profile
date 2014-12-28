9417a553cc339f02451e315fb73b123912bbdbec
Kimmo Koskinen <kimmo@tiuhti.net>
2014-12-28 21:49:18 +0200

    Use 0.8.2 instead of snapshot

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:shared {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]
+{:shared {:plugins [[cider/cider-nrepl "0.8.2"]]
           :repl-options {:init (set! *print-length* 42)}}
  :vinyasa {:dependencies [[im.chit/vinyasa "0.2.2"]
                           [im.chit/iroh "0.1.11"]]
