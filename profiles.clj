be93d9215197460a8a2b30081a923cf247fc83e3
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2014-12-29 09:39:14 +0200

    Add tools.namespace, doc/source/pst to core, formatting

--- profiles.clj
+++ profiles.clj
@@ -2,15 +2,17 @@
                     [lein-pprint "1.1.1"]]
           :repl-options {:init (set! *print-length* 42)}}
  :vinyasa {:dependencies [[im.chit/vinyasa "0.2.2"]
-                          [im.chit/iroh "0.1.11"]]
+                          [im.chit/iroh "0.1.11"]
+                          [org.clojure/tools.namespace "0.2.7"]]
            :injections [(require '[vinyasa.inject :as inject])
-                        (inject/in
-                         [vinyasa.inject :refer [inject [in inject-in]]]
-                         [vinyasa.pull :all]
-                         [cemerick.pomegranate add-classpath get-classpath resources]
-                         clojure.core
-                         [iroh.core .> .? .* .% .%>]
-                         clojure.core >
-                         [clojure.pprint pprint]
-                         [clojure.java.shell sh])]}
+                        (inject/in [vinyasa.inject :refer [inject [in inject-in]]]
+                                   [vinyasa.pull :all]
+                                   [cemerick.pomegranate add-classpath get-classpath resources]
+                                   [clojure.tools.namespace.repl refresh]
+                                   clojure.core
+                                   [iroh.core .> .? .* .% .%>]
+                                   clojure.core >
+                                   [clojure.pprint pprint]
+                                   [clojure.java.shell sh]
+                                   [clojure.repl doc source pst])]}
  :user [:shared :vinyasa]}
