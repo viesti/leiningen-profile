1dde507ae82a95aec5472bbf0b7cda38f1f10c0d
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-04-11 19:38:59 +0300

    Add aprint

--- profiles.clj
+++ profiles.clj
@@ -5,7 +5,8 @@
           :repl-options {:init (set! *print-length* 42)}}
  :vinyasa {:dependencies [[im.chit/vinyasa "0.2.2"]
                           [im.chit/iroh "0.1.11"]
-                          [org.clojure/tools.namespace "0.2.7"]]
+                          [org.clojure/tools.namespace "0.2.7"]
+                          [aprint "0.1.3"]]
            :injections [(require '[vinyasa.inject :as inject])
                         (inject/in [vinyasa.inject :refer [inject [in inject-in]]]
                                    [vinyasa.pull :all]
@@ -15,6 +16,7 @@
                                    [iroh.core .> .? .* .% .%>]
                                    clojure.core >
                                    [clojure.pprint pprint]
+                                   [aprint.core aprint]
                                    [clojure.java.shell sh]
                                    [clojure.repl doc source pst])]}
  :user [:shared :vinyasa]}
