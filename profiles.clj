19daa8f48b39e056126fd33d504e1685f4d7a3c9
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-10-03 19:56:17 +0300

    Use stable versions, update tools.namespace

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:shared {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+{:shared {:plugins [[cider/cider-nrepl "0.9.1"]
                     [lein-pprint "1.1.1"]
                     [jonase/eastwood "0.2.1"]
                     [lein-ancient "0.6.7"]
@@ -9,7 +9,7 @@
           :test-refresh {:growl true}}
  :vinyasa {:dependencies [[im.chit/vinyasa "0.2.2"]
                           [im.chit/iroh "0.1.11"]
-                          [org.clojure/tools.namespace "0.2.10"]
+                          [org.clojure/tools.namespace "0.2.11"]
                           [aprint "0.1.3"]
                           [spyscope "0.1.5"]]
            :injections [(require '[vinyasa.inject :as inject]
@@ -22,11 +22,11 @@
                                    [iroh.core .> .? .* .% .%>]
                                    clojure.core >
                                    [clojure.pprint pprint]
-                                   [aprint.core aprint nprint]
                                    [clojure.java.shell sh]
                                    [clojure.repl doc source pst])]}
  :droid {:plugins [[lein-droid "0.4.0-alpha4"]]}
  :moar-opts {:jvm-opts ["-XX:+UnlockDiagnosticVMOptions" "-XX:+DebugNonSafepoints" "-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder"]}
  :yagni {:plugins [[venantius/yagni "0.1.2"]]}
- :refactor {:plugins [[refactor-nrepl "1.2.0-SNAPSHOT"]]}
+ :refactor {:plugins [[refactor-nrepl "1.1.0"]]}
  :user [:shared :vinyasa :moar-opts :refactor]}
+
