a69699964d6fae828fa2a1ca340a0543892d2ca6
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-04-18 10:00:17 +0300

    Bump up dependencies, try to squish dependency hell

--- profiles.clj
+++ profiles.clj
@@ -1,17 +1,16 @@
-{:shared {:plugins [[cider/cider-nrepl "0.9.1"]
-                    [lein-pprint "1.1.1"]
-                    [jonase/eastwood "0.2.1"]
-                    [lein-ancient "0.6.7"]
-                    [org.clojure/tools.nrepl "0.2.10"]
-                    [com.jakemccrary/lein-test-refresh "0.10.0"]]
+{:shared {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                    [jonase/eastwood "0.2.3"]
+                    [lein-ancient "0.6.10"]
+                    [org.clojure/tools.nrepl "0.2.12"]
+                    [com.jakemccrary/lein-test-refresh "0.11.0"]]
           :global-vars {*print-length* 100}
           :figwheel {:open-file-command "figwheel-file-opener"}
           :test-refresh {:growl true}}
- :vinyasa {:dependencies [[im.chit/vinyasa "0.2.2"]
+ :vinyasa {:dependencies [[im.chit/vinyasa "0.2.2" :exclusions [org.codehaus.plexus/plexus-utils]]
                           [im.chit/iroh "0.1.11"]
                           [org.clojure/tools.namespace "0.2.11"]
                           [aprint "0.1.3"]
-                          [spyscope "0.1.5"]]
+                          [spyscope "0.1.5" :exclusions [clj-time]]]
            :injections [(require '[vinyasa.inject :as inject]
                                  'spyscope.core)
                         (inject/in [vinyasa.inject :refer [inject [in inject-in]]]
@@ -27,6 +26,6 @@
  :droid {:plugins [[lein-droid "0.4.0-alpha4"]]}
  :moar-opts {:jvm-opts ["-XX:+UnlockDiagnosticVMOptions" "-XX:+DebugNonSafepoints" "-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder"]}
  :yagni {:plugins [[venantius/yagni "0.1.2"]]}
- :refactor {:plugins [[refactor-nrepl "1.1.0"]]}
+ :refactor {:plugins [[refactor-nrepl "2.0.0"]]}
  :user [:shared :vinyasa :moar-opts :refactor]}
 
